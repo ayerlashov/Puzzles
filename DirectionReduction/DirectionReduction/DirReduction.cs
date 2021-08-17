b410d27e65dbb635cd7b9e20960cc17296d529fd
ayerlashov <artyom_yerlashov@outlook.com>
2021-08-17 20:57:40 +0200

    Adds the Direction Reduction solution

---
+++ DirectionReduction/DirectionReduction/DirReduction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DirectionReduction
+{
+    public class DirReductionSolver
+    {
+        public static string[] Reduce(string[] arr) =>
+            arr.Length < 2
+            ? arr
+            : Enumerable.Range(2, arr.Length-1)
+                .Select(i => arr.Length - i)
+                .Aggregate(
+                    new Stack<string>(new[] { arr[arr.Length - 1] }),
+                    (stack, i) =>
+                    {
+                        var dir = arr[i];
+                        if (stack.Count > 0 && (Map(dir[0]) ^ Map(stack.Peek()[0])) == 1)
+                            stack.Pop();
+                        else
+                            stack.Push(dir);
+
+                        return stack;
+                    })
+                .ToArray();
+            
+
+        private static int Map(char c)
+        {
+            switch (char.ToUpper(c))
+            {
+                case 'N':
+                    return 0;
+                case 'S':
+                    return 1;
+                case 'W':
+                    return 2;
+                case 'E':
+                    return 3;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(c));
+            }
+        }
+    }
+}
---
+++ DirectionReduction/DirectionReduction/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DirectionReduction
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            string[] c = new string[] { "EAST", "EAST", "SOUTH", "EAST", "SOUTH", "SOUTH", "SOUTH", "EAST", "EAST", "SOUTH", "NORTH", "NORTH", "NORTH", "SOUTH", "SOUTH" };
+            string[] d = new string[] { "NORTH" };
+            string[] e = new string[] { };
+
+            var resA = DirReductionSolver.Reduce(a);
+            var resB = DirReductionSolver.Reduce(b);
+            var resC = DirReductionSolver.Reduce(c);
+            var resD = DirReductionSolver.Reduce(d);
+            var resE = DirReductionSolver.Reduce(e);
+        }
+    }
+}
