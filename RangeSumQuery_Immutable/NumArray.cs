58d82920ad6772b6b603c28e33061d811da5ef78
ayerlashov <artyom_yerlashov@outlook.com>
2021-08-17 11:06:17 +0200

    Adds solution to RangeSumQuery - Immutable.

---
+++ RangeSumQuery_Immutable/NumArray.cs
@@ -0,0 +1,20 @@
+﻿namespace RangeSumQuery_Immutable
+{
+    public class NumArray
+    {
+        private int[] Sums { get; set; }
+
+        public NumArray(int[] nums)
+        {
+            Sums = new int[nums.Length];
+
+            for (int i = 0, s = 0; i < nums.Length; i++)
+                Sums[i] = (s += nums[i]);
+        }
+
+        public int SumRange(int left, int right) => 
+            left == 0 
+            ? Sums[right] 
+            : Sums[right] - Sums[left - 1];
+    }
+}
---
+++ RangeSumQuery_Immutable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+//LeetCode
+namespace RangeSumQuery_Immutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var n = new NumArray(Enumerable.Range(0, 100).Select(_ => 1).ToArray());
+
+            var a = new [] { -2, 0, 3, -5, 2, -1 };
+
+            var na = new NumArray(a);
+
+            var test1 = na.SumRange(0, 2);
+            var test2 = na.SumRange(2, 5);
+            var test3 = na.SumRange(0, 5);
+        }
+    }
+}
